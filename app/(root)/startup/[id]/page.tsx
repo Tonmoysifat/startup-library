77ebdeb74fd7d950b94c984d5cfc2897168b3f06
Tonmuy Ahamed Sifat <tonmoysifatmd@gmail.com>
2024-11-13 00:09:18 +0600

    added flex-wrap class to root div of author info in startup details page

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -35,7 +35,7 @@ const Page = async ({params}: { params: Promise<{ id: string }> }) => {
                     className="w-full h-auto rounded-xl"
                 />
                 <div className="space-y-5 mt-10 max-w-4xl mx-auto">
-                    <div className="flex-between gap-5">
+                    <div className="flex-between gap-5 flex-wrap">
                         <Link href={`/user/${post.author?._id}`} className="flex gap-2 items-center mb-3">
                             <Image src={post.author?.image} alt="avater"
                                    width={64} height={64}
